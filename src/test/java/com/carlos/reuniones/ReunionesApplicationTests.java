5273b953fe88f1e8ba4c1a16965a6b852ba367c3
Carlos Verdugo <carlos.versa99@gmail.com>
2026-05-21 15:48:47 -0700

    initial commit

---
+++ src/test/java/com/carlos/reuniones/ReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
